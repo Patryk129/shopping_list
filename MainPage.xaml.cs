4279ec772fdb6812e62c3d45490f480885cae9a3
Jakub Chrzanowski <kubek1290pl@gmail.com>
2026-03-18 12:13:34 +0100

    update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,28 @@
-﻿namespace shopping_list
+﻿using CloudKit;
+using SQLite;
+
+namespace shopping_list
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        SQLiteAsyncConnection database;
 
         public MainPage()
         {
             InitializeComponent();
+            
+            //CreateTableAsync<T>();
+            //Table<T>().ToListAsync();
+            //InsertAsync(obiekt);
+            //DeleteAsync(obiekt);
+        }
+        async public Database()
+        {
+            if (database is not null)
+                return;
+
+            database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await database.CreateTableAsync<Products>();
         }
 
     }
